93e7af723ca81bf062d0e12c0199d1ba2c99db8d
Pratiksha221 <44203999+Pratiksha221@users.noreply.github.com>
2022-06-20 17:12:00 +0530

    added GTM
    added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,12 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { Toaster } from "react-hot-toast";
 import { StateContext } from "../context/StateContext";
-
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-WSPVSGL" });
+  }, []);
   return (
     <StateContext>
       <Layout>

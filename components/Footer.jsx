85b365c58e90daff1f9b72655be1f0c94a277dac
Pratiksha221 <44203999+Pratiksha221@users.noreply.github.com>
2022-06-21 17:31:46 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,15 +12,15 @@ const Footer = () => {
     <div className="footer-container">
       <p>2022 V Headphones All rights reserved</p>
       <p className="icons">
-        <Link href="https://www.instagram.com/">
-          <AiFillInstagram />
+        <Link href="https://www.instagram.com/" >
+          <a className="instagram-logo"><AiFillInstagram /></a>
         </Link>
 
         <Link href="https://www.twitter.com/">
-          <AiOutlineTwitter />
+          <a className="twitter-logo"><AiOutlineTwitter /></a>
         </Link>
         <Link href="https://www.facebook.com/">
-          <AiFillFacebook/>
+          <a className="facebook-logo"><AiFillFacebook/></a>
         </Link>
       </p>
     </div>
